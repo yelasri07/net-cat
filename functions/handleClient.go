dd79478ef339b6ee857eb7bb56bc7853e69d6e54
Youssef El Asri <168789623+yelasri07@users.noreply.github.com>
2024-11-11 18:26:05 +0100

    Add files via upload

---
+++ functions/handleClient.go
@@ -0,0 +1,25 @@
+package functions
+
+import "net"
+
+func HandleClient(conn net.Conn) {
+	welcomeMessage := "Welcome to TCP-Chat!\n" +
+		"         _nnnn_\n" +
+		"        dGGGGMMb\n" +
+		"       @p~qp~~qMb\n" +
+		"       M|@||@) M|\n" +
+		"       @,----.JM|\n" +
+		"      JS^\\__/  qKL\n" +
+		"     dZP        qKRb\n" +
+		"    dZP          qKKb\n" +
+		"   fZP            SMMb\n" +
+		"   HZM            MMMM\n" +
+		"   FqM            MMMM\n" +
+		" __| \".        |\\dS\"qML\n" +
+		" |    .       | ' \\Zq\n" +
+		"_)      \\.___.,|     .'\n" +
+		"\\____   )MMMMMP|   .'\n" +
+		"     -'       --'\n"
+
+	conn.Write([]byte(welcomeMessage))
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"netcat/functions"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		return
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err.Error())
+			continue
+		}
+
+		functions.HandleClient(conn)
+	}
+}
